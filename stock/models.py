160649f915febd14c902ae16e46003ccd8970328
December98 <obbydecember98@gmail.com>
2023-10-16 03:17:07 +0300

    Initial commit

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class StockItem(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+class SoldItem(models.Model):
+    stock_item = models.ForeignKey(StockItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    sold_date = models.DateTimeField(auto_now_add=True)
+
+class AvailableStock(models.Model):
+    stock_item = models.ForeignKey(StockItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.stock_item.name} - {self.quantity} available"
